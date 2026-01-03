757bdce87c9ab81df8be86c2f78b2f2a24a74f8a
akshara-19274 <gakshara19274@gmail.com>
2026-01-03 19:04:45 +0530

    Structuring

--- restaurentmanagemnetapp/src/App.jsx
+++ restaurentmanagemnetapp/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import {Routes,Route,Navigate} from "react-router-dom"
+import Login from "./Login.jsx"
+import AdminDashboard from "./AdminDashboard.jsx"
+import CustomerDashboard from "./CustomerDashboard.jsx"
+import UpdateRestaurant from "./UpdateRestaurant.jsx"
+import ProtectedRoute from "./ProtectedRoute.jsx"
+export default function App(){
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/admin/dashboard" element={
+        <ProtectedRoute requiredRole="admin">
+          <AdminDashboard />
+        </ProtectedRoute>
+      } />
+      <Route path="/customers/dashboard" element={
+        <ProtectedRoute requiredRole="customer">
+          <CustomerDashboard />
+        </ProtectedRoute>
+      }
+      />
+      <Route path="/admin/restaurants/update" element={
+        <ProtectedRoute requiredRole="admin">
+          <UpdateRestaurant />
+        </ProtectedRoute>
+      }
+      />
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
+  );
 }
-
-export default App
